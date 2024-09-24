ea3151fa12ec5228b6b82eea482c41c1854caa28
A-Barjesteh <abarjesteh@gmail.com>
2024-09-24 10:59:28 -0700

    Combine react practice with Odin Learning

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import './Home.css';
+
+function Home() {
+  return (
+  <div>
+    <head>
+      <meta charset="UTF-8" />
+     <title> This is cool </title>
+    </head>
+
+    <body>
+      <h1> Break yourselves upon my body </h1>
+      <iframe width="1280" height="720" src="https://www.youtube.com/embed/qm-I9i8rFhM" title="My Body ft Ozruk (Mr Voletron Mashed Up 2013)" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+    </body>
+
+    <ul>
+      <li>Break</li>
+      <li>Your</li>
+      <li>Self</li>
+    </ul>
+
+    <ol>
+      <li>Upon</li>
+      <li>My</li>
+      <li>Body</li>
+    </ol>
+  </div>
+  );
+}
+
+export default Home;
