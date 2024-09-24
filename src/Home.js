32f00d1081ad38492c86343b37484f19f550dad2
A-Barjesteh <abarjesteh@gmail.com>
2024-09-24 11:34:04 -0700

    Fix title and add link

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,10 @@ import './Home.css';
 function Home() {
   return (
   <div>
-    <head>
-      <meta charset="UTF-8" />
-     <title> This is cool </title>
-    </head>
-
     <body>
       <h1> Break yourselves upon my body </h1>
+      <a href="https://www.theodinproject.com/about" target="_blank" rel="noopener noreferrer">About Odin</a>
+      <br></br>
       <iframe width="1280" height="720" src="https://www.youtube.com/embed/qm-I9i8rFhM" title="My Body ft Ozruk (Mr Voletron Mashed Up 2013)" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
       </iframe>
     </body>
